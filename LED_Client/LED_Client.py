6a0d1065a3b8d66f872e5e738edd644aef741af6
Jonathan Levin <jon.levin25@gmail.com>
2021-01-15 03:01:18 +0200

    uncommited local client test

--- LED_Client/LED_Client.py
+++ LED_Client/LED_Client.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from asyncio import Future
+from CONSTS import REMOTE_URL
 
 from tornado.ioloop import IOLoop, PeriodicCallback
 from tornado import gen
 from tornado.websocket import websocket_connect, WebSocketHandler
 import asyncio
-Future = asyncio.Future
+import tornado.ioloop
 import typing
-
-ROUTE = "ws://192.168.1.106:8888/leds"
 
 class Client(object):
     def __init__(self, url, timeout):
         self.url = url
         self.timeout = timeout
-        self.ioloop = IOLoop.instance()
         self.ws = None
-        self.connect()
         PeriodicCallback(self.keep_alive, 20000).start()
-        self.ioloop.start()
 
     @gen.coroutine
     def connect(self) -> "Future[WebSocketHandler]":
-        print("trying to connect")
+        print(f"trying to connect to {REMOTE_URL}")
         try:
             self.ws = yield websocket_connect(self.url)
         except Exception as e:
@@ -30,6 +27,7 @@ class Client(object):
         else:
             print("connected")
             self.run()
+            return self.ws
 
     @gen.coroutine
     def run(self):
@@ -48,10 +46,19 @@ class Client(object):
 
 # TODO: test this
 async def connect_ws() -> WebSocketHandler:
-    client = Client(ROUTE, 5)
+    client = Client(REMOTE_URL, 5)
     return await client.connect()
 
+async def main():
+    ws = await connect_ws()
+    await ws.write_message("brightness:3,5,")
+
+async def test():
+    await asyncio.sleep(5)
+    print('whoop')
+
 if __name__ == "__main__":
-    ws = connect_ws()
-    ws.write_message("cycle:3")
+    # IOLoop.instance().run_sync(main) # tornado method
+    asyncio.get_event_loop().run_until_complete(main()) # asyncio method
+
 
