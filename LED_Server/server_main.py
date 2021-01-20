bbad114a2beffa3f2d45b34d9d611d4531dffd2f
Jonathan Levin <jon.levin25@gmail.com>
2021-01-20 23:00:25 +0200

    separate + tidy

--- LED_Server/server_main.py
+++ LED_Server/server_main.py
@@ -12,7 +12,39 @@ from led_actions.actions.BulgeLedAction import Bulge
 import atexit
 
 
-def init() -> Tuple[NeoPixel, PixelsActionsRouter]:
+def run_test_code(pixels: NeoPixel, router: PixelsActionsRouter):
+    golden_yellow = html_to_rgb_bytes("#ff9900")
+    lehe_jon_first_draw_theme = html_to_rgb_bytes("#33ccff")
+    purple = html_to_rgb_bytes("#792d9f")
+    START_COL = COL_RED
+
+    Actions_Basic.autoshow(pixels, True)
+    pixels.brightness = 1
+    pixels.fill(START_COL)
+
+    ### ReadLight
+    # Actions_Basic.set_range(pixels, slice(5, 10), (255, 180, 0))
+    # pixels.brightness = 0.5
+    # return
+
+    # prange = NeoPixelRange(pixels, slice(3, len(pixels), 4))
+    # prange.set_colors(COL_BLACK)
+    # prange.show()
+
+    # set_colors_action = lambda: Actions_Basic.set_sequential(pixels, 0, 0.1)
+    # fillgapsaction = FillGapsAction(pixels, set_colors_action,
+    #     fill_length=3, gap_length=1, half_cycle_time=0.2,
+    #     min_brightness=0.2, max_brightness=1.0, on_halfcycle=None)
+
+    router.add_actions([
+        # fillgapsaction,
+        # Actions_Breathe.bright_pingpong_2(pixels, 1.5),
+        # Actions_ColorCycle.colorcylce(pixels, 6),
+        Bulge(3, pixels, START_COL, rand_deep_color, 0.0055),  # TODO: Easing
+    ])
+
+
+def init_app() -> Tuple[NeoPixel, PixelsActionsRouter]:
     print('initializing pixels')
     pixels = event_loop.init_pixels(30)
     router = PixelsActionsRouter(pixels, {
@@ -25,11 +57,14 @@ def init() -> Tuple[NeoPixel, PixelsActionsRouter]:
     run_test_code(pixels, router)
     atexit.register(lambda: on_app_exit(pixels))
 
+    print(f'listening on port {PORT}')
+    ioloop = IOLoop.current()
+    ioloop.run_sync(lambda: event_loop.event_loop(router.pixels, router.running_actions))
+
     return pixels, router
 
 
-def make_app():
-    pixels, router = init()
+def make_tornado_app(pixels: NeoPixel, router: PixelsActionsRouter):
     application = tornado.web.Application([
         (DISCOVERY_PATH, DiscoveryService, dict(actions_router=router)),
         (ACTIONS_PATH, ActionsService, dict(actions_router=router)),
@@ -37,9 +72,6 @@ def make_app():
     ])
 
     application.listen(PORT)
-    print(f'listening on port {PORT}')
-    ioloop = IOLoop.current()
-    ioloop.run_sync(lambda: event_loop.event_loop(router.pixels, router.running_actions))
     # ioloop.start()
 
 
@@ -49,44 +81,6 @@ def on_app_exit(pixels):
     print('Exiting...')
 
 
-def run_test_code(pixels: NeoPixel, router: PixelsActionsRouter):
-    golden_yellow = html_to_rgb_bytes("#ff9900")
-    lehe_jon_first_draw_theme = html_to_rgb_bytes("#33ccff")
-    purple = html_to_rgb_bytes("#792d9f")
-    START_COL = purple
-
-    Actions_Basic.autoshow(pixels, True)
-
-    ### ReadLight
-    # Actions_Basic.set_range(pixels, slice(5, 10), (255, 180, 0))
-    # pixels.brightness = 0.5
-    # return
-
-    pixels.brightness = 1
-    pixels.fill(START_COL)
-
-    # prange = NeoPixelRange(pixels, slice(3, len(pixels), 4))
-    # prange.set_colors(COL_BLACK)
-    # prange.show()
-
-    # Actions_Basic.set_random(pixels, rand_func_max_colors(3, rand_deep_color))
-    Actions_Basic.set_sequential(pixels)
-    # set_colors_action = lambda: Actions_Basic.set_sequential(pixels, 0, 0.1)
-    # fillgapsaction = FillGapsAction(pixels, set_colors_action,
-    #                                 fill_length=3,
-    #                                 gap_length=1,
-    #                                 half_cycle_time=0.2,
-    #                                 min_brightness=0.2,
-    #                                 max_brightness=1.0,
-    #                                 on_halfcycle=None)
-
-    router.add_actions([
-        # fillgapsaction,
-        # Actions_Breathe.bright_pingpong_2(pixels, 1.5),
-        # Actions_ColorCycle.colorcylce(pixels, 6),
-        Bulge(3, pixels, START_COL, rand_deep_color, 0.0055),  # TODO: Easing
-    ])
-
-
 if __name__ == "__main__":
-    make_app()
+    pixels, router = init_app()
+    make_tornado_app(pixels, router)
