24b6799df67c74a80a71aa6403e3b947924168f1
Jonathan Levin <jon.levin25@gmail.com>
2021-02-09 21:31:35 +0200

    fix typing

--- LED_Server/server_main.py
+++ LED_Server/server_main.py
@@ -12,13 +12,24 @@ from led_actions.actions.BulgeLedAction import Bulge
 import atexit
 
 from led_actions.actions.FillGapsAction import FillGapsAction
+from led_actions.base.LedAction import LedAction
 
+
+def fill_gaps_action(pixels: NeoPixel, fill_length: int, gap_length: int) -> LedAction:
+    set_colors_action = lambda: Actions_Basic.set_sequential(pixels, 0, 0.1)
+    return FillGapsAction(pixels, set_colors_action,
+                                    fill_length=fill_length, gap_length=gap_length, half_cycle_time=3,
+                                    min_brightness=0.2, max_brightness=1.0)
 
 def run_test_code(pixels: NeoPixel, router: PixelsActionsRouter):
     golden_yellow = html_to_rgb_bytes("#ff9900")
     lehe_jon_first_draw_theme = html_to_rgb_bytes("#33ccff")
     purple = html_to_rgb_bytes("#792d9f")
     START_COL = COL_RED
+
+    pixels.fill((0, 255, 0))
+    pixels.show()
+
 
     pixels.brightness = 1
     pixels.fill(START_COL)
@@ -28,14 +39,11 @@ def run_test_code(pixels: NeoPixel, router: PixelsActionsRouter):
     # pixels.brightness = 0.5
     # return
 
-    set_colors_action = lambda: Actions_Basic.set_sequential(pixels, 0, 0.1)
-    fillgapsaction = FillGapsAction(pixels, set_colors_action,
-        fill_length=3, gap_length=2, half_cycle_time=3,
-        min_brightness=0.2, max_brightness=1.0)
 
     router.add_actions([
-        fillgapsaction,
+        # fill_gaps_action(3, 2),
         # Actions_Breathe.bright_pingpong_2(pixels, 1.5),
+        Actions_Breathe.breathe_rand(pixels, rand_func_max_colors(2, rand_deep_color), 1.5),
         # Actions_ColorCycle.colorcylce(pixels, 6),
         # Bulge(3, pixels, START_COL, rand_deep_color, 0.0055),  # TODO: Easing
     ])
@@ -49,6 +57,7 @@ def init_app() -> Tuple[NeoPixel, PixelsActionsRouter]:
         'rand_color': test_fill_rand,
         'set_sequential': Actions_Basic.set_sequential,
         'color_cycle': Actions_ColorCycle.colorcylce,
+        'fill_gaps_action': fill_gaps_action,
     })
 
     run_test_code(pixels, router)
